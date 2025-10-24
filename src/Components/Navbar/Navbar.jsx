5b5842b5dcc296a18c3a56e7c1df6c3a2e4f0719
Keerthantunkoju2001 <Keerthantunkoju10@gmail.com>
2025-10-24 16:13:20 +0530

    v7

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -70,11 +70,12 @@ const Navbar = () => {
           {menu === "kids" ? <hr /> : <></>}
         </li>
       </ul>
-      <div className="nav-login-cart">
-        <Link to="/login">
-          <button>Login</button>
-        </Link>
 
+      <div className="nav-login-cart">
+        {localStorage.getItem('auth-token') ? <button onClick={() => { localStorage.removeItem('auth-token'); window.location.replace('/')}}>Logout</button>:<Link to="/login">
+          <button>Login</button>
+        </Link>}
+        
         <Link to="/cart">
           <img src={cart_icon} alt="" />
         </Link>
--- src/Components/Popular/Popular.jsx
+++ src/Components/Popular/Popular.jsx
@@ -1,9 +1,17 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './Popular.scss'
-import data_product from '../Assets/data'
 import Item from "../Item/Item"
 
 const Popular = () => {
+
+  const [popularProducts, setPopularProducts] = useState([]);
+
+  useEffect(() => {
+    fetch('http://localhost:4000/popularinwomen')
+      .then((response) => response.json())
+      .then((data) => setPopularProducts(data));
+  }, [])
+  
   return (
     <div className="popular">
       <h1>
@@ -11,7 +19,7 @@ const Popular = () => {
       </h1>
       <hr />
       <div className="popular-item">
-        {data_product.map((item,i)=>{
+        {popularProducts.map((item,i)=>{
             return <Item key={i} id={item.id} name={item.name} image={item.image} new_price={item.new_price} old_price={item.old_price} />
         })}
       </div>
